d2e8a5cc02ab89a9a896b7532cbeaf61431a8010
gissandrogama <gissandrogama@gmail.com>
2022-01-30 18:39:57 -0300

    feat: add context members

---
+++ priv/repo/migrations/20220130212906_create_members.exs
@@ -0,0 +1,18 @@
+defmodule FindMyPersonal.Repo.Migrations.CreateMembers do
+  use Ecto.Migration
+
+  def change do
+    create table(:members, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :email, :string
+      add :birth_date, :date
+      add :blood, :string
+      add :height, :string
+      add :weight, :string
+
+      timestamps()
+    end
+
+  end
+end
